e585f0b8f005faf13613a3a493e1f480aeb6862f
Jovie <jovie@snagr.com.hk>
2020-10-29 15:48:44 +0000

    Build not minified files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  chainWebpack: config => config.optimization.minimize(false),
   publicPath:
     process.env.NODE_ENV === "production"
       ? "/amazon-ses-template-management-page"
